a60f63786578f4d50dde3dc2402c75e99fcec13f
fmogollonr <felipemogollonr@gmail.com>
2025-08-28 09:22:37 +0200

    Update trello2markdown.py

--- trello2markdown.py
+++ trello2markdown.py
@@ -139,91 +139,94 @@ answers=r.json()
 #print(r.json())
 
 for answer in answers:
-    #print(answer['id'])
-    #print(answer['name'])
-    output_folder = os.path.join(path, answer["name"])
     try:
-        if os.path.exists(output_folder) is False:
-            os.mkdir(output_folder)
-    except OSError as error:
-        print(error)
-    list_url="https://api.trello.com/1/boards/"+answer['id']+"/lists?key="+key+"&token="+token
-    l=requests.get(list_url)
-    answers_list=l.json()
-    for answer_list in answers_list:
-        #print(answer_list['id'])
-        #print(answer_list['name'])
-
-
-        sublist_url="https://api.trello.com/1/lists/"+answer_list['id']+"/cards?key="+key+"&token="+token+"&attachments=true"
-        #print(card_url)
-        s=requests.get(sublist_url)
-        #print("sublist")
-        sublists_list=s.json()
-        output_file=os.path.join(output_folder, answer_list['name']+".md")
+        #print(answer['id'])
+        #print(answer['name'])
+        output_folder = os.path.join(path, answer["name"])
         try:
-            os.remove(output_file)
-        except:
-            print("doesnt exists") 
+            if os.path.exists(output_folder) is False:
+                os.mkdir(output_folder)
+        except OSError as error:
+            print(error)
+        list_url="https://api.trello.com/1/boards/"+answer['id']+"/lists?key="+key+"&token="+token
+        l=requests.get(list_url)
+        answers_list=l.json()
+        for answer_list in answers_list:
+            #print(answer_list['id'])
+            #print(answer_list['name'])
 
-        for card in sublists_list:
-            print(card['id'])
+
+            sublist_url="https://api.trello.com/1/lists/"+answer_list['id']+"/cards?key="+key+"&token="+token+"&attachments=true"
+            #print(card_url)
+            s=requests.get(sublist_url)
+            #print("sublist")
+            sublists_list=s.json()
             output_file=os.path.join(output_folder, answer_list['name']+".md")
-            f = open(output_file, "a")
-            f.write("# "+card['name']+"\n")
-            f.close()
-            f = open(output_file, "a")
-            f.write(card['desc'])
-            if len(card['labels']) > 0:
-                f.write('\n## Etiquetas\n')
-            for etiqueta in card['labels']:
-                #print(etiqueta['name'])
-                f.write("* "+etiqueta['name']+"\n")
-            #print(card['attachments'])
-            #print("get actions")
-            actions_url="https://api.trello.com/1/cards/"+card['id']+"/actions?key="+key+"&token="+token
-                    #print(url)
-            actions_request=requests.get(actions_url)
-            actions=parsed = json.loads(actions_request.content)
-            for action in actions:
-                if action['type'] == "commentCard":
-                    if action['data']['card']['id'] == card['id']:
-                        f.write('\n## Comentario el '+action['date']+' por '+action['memberCreator']['fullName']+'\n')
-                        f.write(action['data']['text']+'\n')
+            try:
+                os.remove(output_file)
+            except:
+                print("doesnt exists") 
 
-            if len(card['attachments']) > 0:
-                #print("adjuntos")
-                adjuntos_folder = os.path.join(path,"adjuntos")
-                try:
-                    os.mkdir(adjuntos_folder)
-                except OSError as error:
-                    print(error)
-                attach_request="https://api.trello.com/1/cards/"+card['id']+"/attachments/?fields=url&key="+key+"&token="+token
-                adjunto_respuesta=requests.get(attach_request)
-                parsed = json.loads(adjunto_respuesta.content)
-                #print(parsed)
-                for adjunto in parsed:
-                    print(adjunto['url'])
-                    url="https://api.trello.com/1/cards/"+card['id']+"/attachments/"+adjunto['id']+"?key="+key+"&token="+token
-                    url=re.sub("trello.com","api.trello.com",adjunto['url'])
-                    url2=url+"?key="+key+"&token="+token
-                    descarga = requests.get(re.sub("trello.com","api.trello.com",adjunto['url']))
-                    adjunto_name= adjunto['url'].split("/")
-                    adjunto_nombre=re.sub('[^a-zA-Z0-9\.]','_', adjunto_name[len(adjunto_name)-1])
-                    output_file_adjunto=os.path.join(adjuntos_folder, adjunto_nombre)
-                    print('Saving attachment', adjunto_nombre)
+            for card in sublists_list:
+                print(card['id'])
+                output_file=os.path.join(output_folder, answer_list['name']+".md")
+                f = open(output_file, "a")
+                f.write("# "+card['name']+"\n")
+                f.close()
+                f = open(output_file, "a")
+                f.write(card['desc'])
+                if len(card['labels']) > 0:
+                    f.write('\n## Etiquetas\n')
+                for etiqueta in card['labels']:
+                    #print(etiqueta['name'])
+                    f.write("* "+etiqueta['name']+"\n")
+                #print(card['attachments'])
+                #print("get actions")
+                actions_url="https://api.trello.com/1/cards/"+card['id']+"/actions?key="+key+"&token="+token
+                        #print(url)
+                actions_request=requests.get(actions_url)
+                actions=parsed = json.loads(actions_request.content)
+                for action in actions:
+                    if action['type'] == "commentCard":
+                        if action['data']['card']['id'] == card['id']:
+                            f.write('\n## Comentario el '+action['date']+' por '+action['memberCreator']['fullName']+'\n')
+                            f.write(action['data']['text']+'\n')
+
+                if len(card['attachments']) > 0:
+                    #print("adjuntos")
+                    adjuntos_folder = os.path.join(path,"adjuntos")
                     try:
-                        content = requests.get(url2,stream=True,timeout=60)
-                        #print(content.request.headers)
-                    except Exception:
-                        print("Error descargando")
-                        continue
-                    try:
-                        with open(adjunto_nombre, 'wb') as f:
-                            for chunk in content.iter_content(chunk_size=1024):
-                                if chunk:
-                                    f.write(chunk)
-                    except IOError:
-                        print("Error")
+                        os.mkdir(adjuntos_folder)
+                    except OSError as error:
+                        print(error)
+                    attach_request="https://api.trello.com/1/cards/"+card['id']+"/attachments/?fields=url&key="+key+"&token="+token
+                    adjunto_respuesta=requests.get(attach_request)
+                    parsed = json.loads(adjunto_respuesta.content)
+                    #print(parsed)
+                    for adjunto in parsed:
+                        print(adjunto['url'])
+                        url="https://api.trello.com/1/cards/"+card['id']+"/attachments/"+adjunto['id']+"?key="+key+"&token="+token
+                        url=re.sub("trello.com","api.trello.com",adjunto['url'])
+                        url2=url+"?key="+key+"&token="+token
+                        descarga = requests.get(re.sub("trello.com","api.trello.com",adjunto['url']))
+                        adjunto_name= adjunto['url'].split("/")
+                        adjunto_nombre=re.sub('[^a-zA-Z0-9\.]','_', adjunto_name[len(adjunto_name)-1])
+                        output_file_adjunto=os.path.join(adjuntos_folder, adjunto_nombre)
+                        print('Saving attachment', adjunto_nombre)
+                        try:
+                            content = requests.get(url2,stream=True,timeout=60)
+                            #print(content.request.headers)
+                        except Exception:
+                            print("Error descargando")
+                            continue
+                        try:
+                            with open(adjunto_nombre, 'wb') as f:
+                                for chunk in content.iter_content(chunk_size=1024):
+                                    if chunk:
+                                        f.write(chunk)
+                        except IOError:
+                            print("Error")
             
-            f.close()
+                f.close()
+    except:
+        print ("Error general")
